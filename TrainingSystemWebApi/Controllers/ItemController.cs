ea8b818813f2bdf0f3d682dfd0ee0bec40fd2030
YukiAKubi <kazuyuki19991207@gmail.com>
2023-06-02 19:48:49 +0900

    プロジェクトを整形

---
+++ TrainingSystemWebApi/Controllers/ItemController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrainingSystemWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController
+    {
+    }
+}
---
+++ TrainingSystemWebApi/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using TrainingSystemLibrary.Common;
+
+namespace TrainingSystemWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController : ControllerBase
+    {
+    }
+}
